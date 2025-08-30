b1c7a9777fc3535977a7e9501c603392284d9135
Falcon-30 <parmarjainu25@gmail.com>
2025-08-30 16:56:50 +0530

    springWeb Controller added

---
+++ src/main/java/org/example/ci_cd_argo_cd/CiCdArgoCdApplication.java
@@ -0,0 +1,13 @@
+package org.example.ci_cd_argo_cd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CiCdArgoCdApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CiCdArgoCdApplication.class, args);
+    }
+
+}
---
+++ src/main/java/org/example/ci_cd_argo_cd/controller/CiCdController.java
@@ -0,0 +1,15 @@
+package org.example.ci_cd_argo_cd.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("ci-cd/")
+public class CiCdController {
+    @GetMapping
+    public ResponseEntity<String> helloController(){
+        return ResponseEntity.ok("Hello World");
+    }
+}
