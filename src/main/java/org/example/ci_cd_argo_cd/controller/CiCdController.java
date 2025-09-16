f25ab51b6f87584b3c6391c0c96727f8d498a601
Jainish Parmar <110287786+jainishParmar@users.noreply.github.com>
2025-09-16 22:38:03 +0530

    Update CiCdController.java

--- src/main/java/org/example/ci_cd_argo_cd/controller/CiCdController.java
+++ src/main/java/org/example/ci_cd_argo_cd/controller/CiCdController.java
@@ -6,20 +6,25 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("ci-cd")
+@RequestMapping("ci-cd/")
 public class CiCdController {
     @GetMapping
     public ResponseEntity<String> helloController(){
         return ResponseEntity.ok("Auto sync working");
     }
 
-    @GetMapping("/notification")
+    @GetMapping("notification")
     public ResponseEntity<String> NotificationController(){
         return ResponseEntity.ok("ArgoCd Notification Working");
     }
 
-      @GetMapping("/auto")
+      @GetMapping("auto")
     public ResponseEntity<String> autoSyncController(){
         return ResponseEntity.ok("ArgoCd autosync Working");
+    }
+
+         @GetMapping("argo-noti")
+    public ResponseEntity<String> autoSyncController(){
+        return ResponseEntity.ok("ArgoCd email notification Working");
     }
 }
