dc44406e274c2ac9f8d70117165b2d7f2537bb99
Jainish Parmar <110287786+jainishParmar@users.noreply.github.com>
2025-09-21 11:35:21 +0530

    configure Web Hook

--- src/main/java/org/example/ci_cd_argo_cd/controller/CiCdController.java
+++ src/main/java/org/example/ci_cd_argo_cd/controller/CiCdController.java
@@ -38,6 +38,12 @@ public class CiCdController {
         return ResponseEntity.ok("Everything Working Properly");
     }
 
+         @GetMapping("webhooktest")
+    public ResponseEntity<String> webhookTestController(){
+        return ResponseEntity.ok("webHook Working Properly");
+    }
+
+
     
     
 
